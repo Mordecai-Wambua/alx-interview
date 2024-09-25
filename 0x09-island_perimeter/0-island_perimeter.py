3de14b4de747490e61c81fba523445f21b82e5d1
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-09-25 15:27:34 +0300

    0. Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+"""Function to calculate island perimeter."""
+
+
+def island_perimeter(grid):
+    """Return the perimeter of the island described in grid."""
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+    return perimeter
